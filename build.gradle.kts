1272e205a03b27bb1c58852fa00fe055165b43e3
Roland Ringgenberg <roland@rolandringgenberg.com>
2024-08-01 18:32:53 +0200

    Updated structure to how gradle is looking now

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,65 +1,10 @@
-@Suppress("DSL_SCOPE_VIOLATION")  // needed for the new gradle libs.versions.toml magic
 plugins {
-    alias(libs.plugins.kotlin.multiplatform) apply false
+    // Following plugins are added by Amper already
+    //alias(libs.plugins.androidApplication) apply false
+    //alias(libs.plugins.androidLibrary) apply false
+    //alias(libs.plugins.jbComposeCompiler) apply false
+    //alias(libs.plugins.jbComposeMultiplatform) apply false
+    //alias(libs.plugins.kotlinMultiplatform) apply false
+
     alias(libs.plugins.kotest.multiplatform) apply false
-}
-
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.21.0")
-    }
-}
-
-subprojects {
-
-    group = "ch.rrte.kool.playground"
-    version = "0.1.0"
-
-    repositories {
-        mavenLocal()
-        google()
-        mavenCentral()
-        maven("https://oss.sonatype.org/content/repositories/snapshots") // for Kool SNAPSHOTS
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
-
-    //tasks.withType<KotlinCompile> {
-    //    kotlinOptions {
-    //        jvmTarget = JavaVersion.VERSION_17.majorVersion
-    //    }
-    //}
-
-    // Run all JVM tests with JUnit5
-    tasks.withType<Test>().configureEach {
-        useJUnitPlatform()
-    }
-
-}
-
-// Example to configure jvm tests to show passing desktop tests in the build
-/*
-tasks.named<Test>("desktopTest") {
-    useJUnitPlatform()
-    filter {
-        isFailOnNoMatchingTests = false
-    }
-    testLogging {
-        showExceptions = true
-        showStandardStreams = true
-        events = setOf(
-            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
-            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
-        )
-        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-    }
-}
-
- */
-
-// To delete the top build folder
-tasks.withType<Delete>() {
-    delete(rootProject.buildDir)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,40 @@
 rootProject.name = "pg-games-kool-playground"
-include(
-    ":app:desktop:desktopApp",
-    ":app:kool:koolApp",
-    ":app:web:webApp"
-)
-include(
-    ":modules:core:core-ui",
-)
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+    }
+}
+
+// Following block is only active for gradle and is not taken into account for Amper
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+plugins {
+    // apply the Amper plugin:
+    id("org.jetbrains.amper.settings.plugin").version("0.3.1")
+}
+
+include("composeApp")
+include("koolApp")
