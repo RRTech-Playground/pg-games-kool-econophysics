672f8b2b2a5b5a75663d1cca1b3c9312f067750f
Roland Ringgenberg <roland@rolandringgenberg.com>
2024-08-01 20:35:03 +0200

    Working Compose 1.7.0 App

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times in each subproject's classloader
 
-//    alias(libs.plugins.androidApplication) apply false
-//    alias(libs.plugins.androidLibrary) apply false
-//    alias(libs.plugins.jbComposeCompiler) apply false
-//    alias(libs.plugins.jbComposeMultiplatform) apply false
-//    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.jbComposeCompiler) apply false
+    alias(libs.plugins.jbComposeMultiplatform) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
 
-    alias(libs.plugins.kotest.multiplatform) apply false
+    alias(libs.plugins.kotestMultiplatform) apply false
+    alias(libs.plugins.realm.kotlin) apply false
 }
